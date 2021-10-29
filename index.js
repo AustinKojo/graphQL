5125ffd5bdf622cd68a725dc09923e80f182f2ea
Austin <82212804+AustinKojo@users.noreply.github.com>
2021-10-29 17:27:56 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const _ = require('lodash')
+
+
+
+const app = express();
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));  
+
+
+
+app.listen(5000, () => {
+    console.log('now listening for requests on port 5000');
+});
+
